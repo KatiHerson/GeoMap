03cca70dadf8f1dfee64e534786989ca08bf653e
Test <Test>
2020-11-24 15:27:01 +0300

    map

---
+++ GeoMap/LocationVC.swift
@@ -0,0 +1,54 @@
+//
+//  LocationVC.swift
+//  GeoMap
+//
+//  Created by user on 24/11/2020.
+//
+
+import UIKit
+import CoreLocation
+
+class LocationVC: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+
+    @IBOutlet weak var dataLat: UILabel!
+    @IBOutlet weak var dataLon: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkAutorization()
+        // Do any additional setup after loading the view.
+    }
+    func checkAutorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            print("Error")
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            break
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last?.coordinate {
+        dataLat.text = String(location.latitude)
+        dataLon.text = String(location.longitude)
+        locationManager.stopUpdatingLocation()
+    }
+ }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
